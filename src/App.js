440edb4c86831c54e9b6b9230516109364cd63d0
Ayush Palak <ayushpalakcs@gmail.com>
2019-08-25 13:10:58 +0530

    added 100 words data. selects randomly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {data} from "./word-data"
 
+let glbword = ""
 function App() {
+  let word = data.split("\n\n")[Math.floor(Math.random(1,100)*100)];  
+  glbword = word;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          First build
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div style={{marginTop:"5%"}}>
+        <h1 style={{color:"white"}}>
+          {glbword.split("\n")[0].split(".")[1]} 
+        </h1>
+        <br></br>
+        <h2>
+        <p style={{display:"inline-block",color:"white"}}>{glbword.split("\n")[2].split(":")[0]} : </p> <p style={{display:"inline-block"}}>{glbword.split("\n")[2].split(":")[1]}</p>
+        </h2>
+        {/* <h3>
+        {glbword.split("\n")[3]} 
+        </h3>
+        <h4>
+        {glbword.split("\n")[4]} 
+        </h4> */}
+        </div>
       </header>
     </div>
   );
