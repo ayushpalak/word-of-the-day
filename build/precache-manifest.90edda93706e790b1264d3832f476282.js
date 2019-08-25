cadfc8957b92227a65964a38fe4945b5ce51edc8
Ayush Palak <ayushpalakcs@gmail.com>
2019-08-25 12:09:25 +0530

    second commit

---
+++ build/precache-manifest.90edda93706e790b1264d3832f476282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d29649548a2c93f49f3b535fbd7624d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "372585b4b533df14270e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8cd5b8f721803fe1b2d0",
+    "url": "/static/js/2.e93e5870.chunk.js"
+  },
+  {
+    "revision": "372585b4b533df14270e",
+    "url": "/static/js/main.674b333d.chunk.js"
+  },
+  {
+    "revision": "0ea9e595b701cd9b8ac0",
+    "url": "/static/js/runtime~main.d2aca4a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          First build
         </a>
       </header>
     </div>
